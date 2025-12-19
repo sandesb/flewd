523930c885f89ca29abca461a548f3cf108c15e0
sandesb <bajracharyasandeshh@gmail.com>
2025-12-19 09:34:22 +0545

    Update dependencies and integrate Redux with Snowfall effect in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store/store'
 import HumanBodyWaterFill from './components/HumanBodyWaterFill'
 import Moon from './components/Moon'
 import { Toaster } from 'react-hot-toast'
+import Snowfall from 'react-snowfall'
 
 function App() {
   return (
-    <Router>
-      <div>
-    
+    <Provider store={store}>
+      <Router>
+        <div style={{ position: 'relative', minHeight: '100vh', width: '100%' }}>
+          <Snowfall 
+            snowflakeCount={600} 
+            color='#2196f3'
+          />
+          {/* Routes */}
+          <Routes>
+            <Route path="/" element={<HumanBodyWaterFill />} />
+            <Route path="/moon" element={<Moon />} />
+          </Routes>
 
-        {/* Routes */}
-        <Routes>
-          <Route path="/" element={<HumanBodyWaterFill />} />
-          <Route path="/moon" element={<Moon />} />
-        </Routes>
-
-        <Toaster />
-      </div>
-    </Router>
+          <Toaster />
+        </div>
+      </Router>
+    </Provider>
   )
 }
 
